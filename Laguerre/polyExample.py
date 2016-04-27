41b88d32fc6491b39a6088f521c41568ae21874f
Ignacio Iker Prado Rujas <iker.prado13@gmail.com>
2016-04-27 13:29:18 +0200

    Added folder with Laguerre's method and folder for LaTeX sources.

---
+++ Laguerre/polyExample.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from polyRoots import *
+import numpy as np
+
+# Example 1: x^4 - 5x^3 - 9x^2 + 155x - 250
+c = np.array([1.0, -5.0, -9.0, 155.0, -250.0])
+rootList = polyRoots(c)
+print('x^4 - 5x^3 - 9x^2 + 155x - 250\nRoots are:\n')
+print '[%s]' % ',\n '.join(map(str, rootList)), '\n'
+
+
+# Example 2: x^7 + 2x^4 + x^3 + x
+c = np.array([1, 0, 0, 2, 1, 0, 1, 0])
+rootList = polyRoots(c)
+print('x^7 + 2x^4 + x^3 + x\nRoots are:\n')
+print '[%s]' % ',\n '.join(map(str, rootList)), '\n'
+
+# Example 2: Delta(x, 0.28^2) = 0.0784*x^6 - x^4 - 1.8432*x^3 - 0.9216*x^2 + 0.0784
+c = np.array([0.0784, 0, -1.0, - 1.8432, - 0.9216, 0, 0.0784])
+rootList = polyRoots(c)
+print('Delta(x, 0.28^2)\nRoots are:\n')
+print '[%s]' % ',\n '.join(map(str, rootList)), '\n'
---
+++ Laguerre/polyRoots.py
@@ -0,0 +1,51 @@
+# module polyRoots
+''' roots = polyRoots(a).
+    Uses Laguerre's method to compute all the roots of
+    a[0]*x^n + a[1] * x^(n-1) + a[2] * x^(n-2) + ... + a[n-1] * x + a[n] = 0.
+    The roots are returned in the array 'roots'.
+'''
+import numpy as np
+import cmath
+from random import random
+
+
+def polyRoots(a, tol=1.0e-12):
+
+    def laguerre(a, tol):
+        x = random()  # Starting value (random number)
+        n = len(a) - 1
+        for i in range(30):
+            p = np.polyval(a, x)
+            dp = np.polyval(np.polyder(a, 1), x)
+            ddp = np.polyval(np.polyder(a, 2), x)
+            if abs(p) < tol:
+                return x
+            g = dp / p
+            h = g * g - ddp / p
+            f = cmath.sqrt((n - 1) * (n * h - g * g))
+            if abs(g + f) > abs(g - f):
+                dx = n / (g + f)
+            else:
+                dx = n / (g - f)
+            x = x - dx
+            if abs(dx) < tol:
+                return x
+        print('Too many iterations...')
+
+    def deflPoly(a, root):  # Deflates a polynomial
+        n = len(a) - 1
+        b = [(0.0 + 0.0j)] * n
+        b[0] = a[0]  # The leading coefficient still unchanged
+        for i in range(1, n):
+            b[i] = a[i] + root * b[i - 1]
+        return b
+
+    n = len(a) - 1
+    roots = np.zeros((n), dtype=complex)
+    for i in range(n):
+        x = laguerre(a, tol)
+        if abs(x.imag) < tol:
+            x = x.real
+        roots[i] = x
+        a = deflPoly(a, x)
+    return roots
